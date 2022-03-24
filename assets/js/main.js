965d2f83c8cb51834935752bdf7c3d3ef4fe3733
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-03-24 16:01:31 +0100

    Funzionalità dell'esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,17 +11,43 @@ Questo richiederà un minimo di ricerca. */
 
 
 // Chiedere numero di km da percorrere
+
 const km_da_percorrere = parseInt(prompt('Inserisci i km da percorrere'));
-console.log(km_da_percorrere);
+/* console.log(km_da_percorrere); */
+
 // Chiedere età del passeggero
 const eta = parseInt(prompt('Inserisci età'));
-console.log(eta);
-// Calcolare prezzo del biglietto con sconto del 20% per i minorenni e del 40% per gli over 65
+/* console.log(eta); */
 
 // definire il costo dei km
-let prezzo_al_km = 0.21 * km_da_percorrere;
-console.log(prezzo_al_km);
+const prezzo_al_km = 0.21;
 
+// Calcolare prezzo del biglietto con sconto del 20% per i minorenni e del 40% per gli over 65
+// Prezzo del biglietto senza sconto
+const prezzo_del_biglietto = prezzo_al_km * km_da_percorrere;
+
+// sconti minorenne e over 65
+const sconto_minorenne = prezzo_del_biglietto * 20 / 100;
+
+const sconto_over_65 = prezzo_del_biglietto * 40 / 100;
+
+// Prezzo biglietto minorenne e over 65
+
+const prezzo_biglietto_minorenne = prezzo_del_biglietto - sconto_minorenne;
+
+const prezzo_biglietto_over_65 = prezzo_del_biglietto - sconto_over_65;
 
 // Visualizzare il prezzo finale del biglietto comprensivo di sconti
-let prezzo_del_biglietto = km_da_percorrere * prezzo_al_km;
+if (eta < 18) {
+    document.getElementById('biglietto').innerHTML = 'Il costo del biglietto è ' + prezzo_biglietto_minorenne + '€';
+    console.log(prezzo_biglietto_minorenne);
+} else if (eta > 64){
+    document.getElementById('biglietto').innerHTML = 'Il costo del biglietto è ' + prezzo_biglietto_over_65 + '€';
+    console.log(prezzo_biglietto_over_65);
+} else {
+    document.getElementById('biglietto').innerHTML = 'Il costo del biglietto è ' + prezzo_del_
+    biglietto + '€';
+    console.log(prezzo_del_biglietto);
+}
+
+
